4a7768db9991186d3954ab1ddfc0e47041e4570e
Brad Leege <bleege@gmail.com>
2015-12-19 21:38:31 -0600

    Adding animateCamera() button example

--- app/src/main/java/com/bradleege/googlemapcamera/MapsActivity.java
+++ app/src/main/java/com/bradleege/googlemapcamera/MapsActivity.java
@@ -2,11 +2,14 @@ package com.bradleege.googlemapcamera;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -22,6 +25,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Button cameraAnimate = (Button) findViewById(R.id.cameraAnimateButton);
+        cameraAnimate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(new LatLng(44.50128, -88.06216))    // Sets the center of the map to Lambeau Field
+                        .zoom(14)                                   // Sets the zoom
+                        .tilt(30)                                   // Sets the tilt of the camera to 30 degrees
+                        .build();                                   // Creates a CameraPosition from the builder
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+
+
+
     }
 
 
